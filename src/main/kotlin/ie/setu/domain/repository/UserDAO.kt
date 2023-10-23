694f8a15b9b27766c48a6f3d7509e5068cf5535a
serenebabu2022 <serenebabu22@gmail.com>
2023-10-23 19:50:57 +0100

    Updated

--- src/main/kotlin/ie/setu/domain/repository/UserDAO.kt
+++ src/main/kotlin/ie/setu/domain/repository/UserDAO.kt
@@ -6,7 +6,6 @@ import ie.setu.utils.mapToUser
 import org.jetbrains.exposed.sql.select
 import org.jetbrains.exposed.sql.selectAll
 import org.jetbrains.exposed.sql.transactions.transaction
-
 class UserDAO {
 
     fun getAll():ArrayList<User>{
