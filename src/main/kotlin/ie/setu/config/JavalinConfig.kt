07b6ba071dc825a5a4db9e9bfb3c55665912fec6
serenebabu2022 <serenebabu22@gmail.com>
2023-12-22 07:45:09 +0000

    Implemented a frontend using Vue

--- src/main/kotlin/ie/setu/config/JavalinConfig.kt
+++ src/main/kotlin/ie/setu/config/JavalinConfig.kt
@@ -7,12 +7,15 @@ import ie.setu.utils.jsonObjectMapper
 import io.javalin.Javalin
 import io.javalin.apibuilder.ApiBuilder.*
 import io.javalin.json.JavalinJackson
+import io.javalin.vue.VueComponent
 
 class JavalinConfig {
     fun startJavalinService(): Javalin {
         val app = Javalin.create {
             // added this jsonMapper for our integration tests - serialise objects to json
             it.jsonMapper(JavalinJackson(jsonObjectMapper()))
+            it.staticFiles.enableWebjars()
+            it.vue.vueAppName = "app" // only required for Vue 3, is defined in layout.html
         }.apply {
             exception(Exception::class.java) { e, _ -> e.printStackTrace() }
             error(404) { ctx -> ctx.json("404 : Not Found") }
@@ -72,6 +75,13 @@ class JavalinConfig {
                     patch(FriendsController::updateFriend)
                 }
             }
+            // The @routeComponent that we added in layout.html earlier will be replaced
+            // by the String inside the VueComponent. This means a call to / will load
+            // the layout and display our <home-page> component.
+            get("/", VueComponent("<home-page></home-page>"))
+            get("/users", VueComponent("<user-overview></user-overview>"))
+            get("/users/{user-id}", VueComponent("<user-profile></user-profile>"))
+            get("/users/{user-id}/activities", VueComponent("<user-activity-overview></user-activity-overview>"))
         }
     }
     private fun getRemoteAssignedPort(): Int {
